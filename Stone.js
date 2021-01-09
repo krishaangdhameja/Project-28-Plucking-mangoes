2a743ba8f816a566a125ef8ed52f9cdcfec020ed
krishaangdhameja <74408476+krishaangdhameja@users.noreply.github.com>
2021-01-09 15:24:08 +0530

    Add files via upload

---
+++ Stone.js
@@ -0,0 +1,19 @@
+class stone {
+    constructor(x,y){
+        var opt = {
+            isStatic : false,
+            restitution : 0,
+            friction : 1,
+            density : 1.2
+        }
+        this.body = Bodies.circle(x,y,40,opt);
+        World.add(world,this.body)
+        this.image = loadImage("stone.png");
+    }
+    display(){
+        
+        imageMode(CENTER);
+        image(this.image,this.body.position.x,this.body.position.y,40,40);
+        
+    }
+}
